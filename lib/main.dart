eba9acebef83436017ebf45142df63aad54b29ec
Lucas Oliveira <lucas.sales@justa.com.vc>
2022-04-26 15:09:51 -0300

    feature: :sparkles: adiciona logo bytebank e botão de contatos

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,25 @@ class Bytebank extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Dashboard'),),
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 100,
+              width: 150,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.people),
+                  Text('Contacts')
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
