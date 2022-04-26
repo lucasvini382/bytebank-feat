a71656cfbdc076c0a810efcd6882a9875f5d1352
Lucas Oliveira <lucas.sales@justa.com.vc>
2022-04-26 16:25:43 -0300

    feature: :sparkles: cria tela de formulário de contatos.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/contact_form.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/cupertino.dart';
@@ -26,7 +27,7 @@ class Bytebank extends StatelessWidget {
             textTheme: ButtonTextTheme.primary
         ),
       ),
-      home: Dashboard(),
+      home: ContactForm(),
     );
   }
 }
