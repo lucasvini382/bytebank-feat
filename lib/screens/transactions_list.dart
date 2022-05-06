64cfef6d7d6f62e4caacf603d1a7a463fe134486
Lucas Oliveira <lucas.sales@justa.com.vc>
2022-05-06 15:14:59 -0300

    feat: :sparkles: cria integração entre transactions list e api

--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,42 +1,59 @@
+import 'package:bytebank/http/web_client.dart';
 import 'package:flutter/material.dart';
-
-import '../models/contact.dart';
+import '../components/progress.dart';
 import '../models/transaction.dart';
 
 class TransactionsList extends StatelessWidget {
-  final List<Transaction> transactions = [];
 
   @override
   Widget build(BuildContext context) {
-    transactions.add(Transaction(100.0, Contact(0, 'Alex', 1000)));
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Transactions'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Transactions'),
+        ),
+        body: FutureBuilder<List<Transaction>>(
+          future:
+              Future.delayed(Duration(milliseconds: 500)).then((value) => findAll()),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                break;
+              case ConnectionState.waiting:
+                return Progress();
+                break;
+              case ConnectionState.active:
+                break;
+              case ConnectionState.done:
+                final List<Transaction> transactions = snapshot.data;
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Transaction transaction = transactions[index];
+                    return Card(
+                      child: ListTile(
+                        leading: Icon(Icons.monetization_on),
+                        title: Text(
+                          transaction.value.toString(),
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          transaction.contact.accountNumber.toString(),
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: transactions.length,
+                );
+                break;
+            }
+
+            return Text('Unknown error');
+          },
+        ));
   }
 }
